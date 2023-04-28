5bfb8a6361e61678bf217d7ac3a97d432bd47c06
AsafHaim147 <asafhaim147@gmail.com>
2023-04-28 20:02:09 +0300

    master

--- deliveries/views.py
+++ deliveries/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from deliveries.models import Cart,Items,Delivery
 from main.forms import SubmitDelivery
-from django import template
 
 
 # Create your views here.
